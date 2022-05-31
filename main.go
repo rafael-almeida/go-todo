02b1d600040c325672e09e9d846841f58059a835
Rafael Almeida <r.almeida094@gmail.com>
2022-05-30 20:36:40 -0400

    add get handlers

--- main.go
+++ main.go
@@ -8,12 +8,30 @@ import (
 
 const CONN_ADR = "localhost:8080"
 
-func helloWorld(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, "Hello, world!")
+type todo struct {
+	Id string
 }
+
+func getTodos(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, todos)
+}
+
+func getTodoById(ctx *gin.Context) {
+	id := ctx.Param("id")
+	for _, t := range todos {
+		if t.Id == id {
+			ctx.JSON(http.StatusOK, t)
+			return
+		}
+	}
+	ctx.JSON(http.StatusNotFound, gin.H{"message": "todo " + id + " was not found"})
+}
+
+var todos = []todo{{Id: "1"}, {Id: "2"}}
 
 func main() {
 	router := gin.Default()
-	router.GET("/", helloWorld)
+	router.GET("/todos", getTodos)
+	router.GET("/todos/:id", getTodoById)
 	router.Run(CONN_ADR)
 }
